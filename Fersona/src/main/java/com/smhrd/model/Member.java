9c49af66a4b242bb888d11e6cbfdc50e463f7721
주혜수 <106126676+juhyesu128@users.noreply.github.com>
2022-09-27 09:35:44 +0900

    우선 로그인 완~! 관리자 계정 id : admin, pw : 123 입니다_주혜수

--- Fersona/src/main/java/com/smhrd/model/Member.java
+++ Fersona/src/main/java/com/smhrd/model/Member.java
@@ -1,5 +1,23 @@
 package com.smhrd.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@NoArgsConstructor // 기본 생성자
+@RequiredArgsConstructor   // final, @NonNUll
+@AllArgsConstructor // 모든 파라미터 받는 생성자
+// 모든 파라미터 받는 생성자
 public class Member {
+
+	@NonNull
+	private String id;
+	@NonNull
+	private String pw;
+	private String name;
+	
 
 }
--- Fersona/src/main/java/com/smhrd/model/MemberDAO.java
+++ Fersona/src/main/java/com/smhrd/model/MemberDAO.java
@@ -1,5 +1,49 @@
 package com.smhrd.model;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.smhrd.database.SqlSessionManager;
+
 public class MemberDAO {
 
+	// 세션을 생성해줄 수 있는 factory 생성
+	SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
+	
+	// 세션 생성(sql 실행, close(), 연결(connection))
+	SqlSession  sqlSession = sqlSessionFactory.openSession();
+	
+	// 회원가입 메서드
+	public int insertMember(Member vo) {
+		int cnt = 0;
+		try {
+			cnt = sqlSession.insert("com.smhrd.model.MemberDAO.insertMember",vo);
+			if(cnt>0) {
+				sqlSession.commit();
+			}else {
+				sqlSession.rollback();
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			sqlSession.close();
+		}
+		return cnt;
+	}
+	
+	
+	// 로그인 메서드
+	public Member selectMember(Member vo) {
+		Member loginMember = null;
+		try {
+			loginMember = sqlSession.selectOne("com.smhrd.model.MemberDAO.selectMember", vo);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			sqlSession.close();
+		}
+		return loginMember;
+	}
+	
+	
 }
