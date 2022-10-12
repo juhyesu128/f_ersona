7a82d215836e8b3cc9cd316b980088d350d4184f
smhrd <smhrd@119.200.31.221>
2022-10-12 17:57:45 +0900

    지은 - 수배자 정보 리스트 기능구현>> 리스트 불러오기

--- Fersona/src/main/java/com/smhrd/model/Want.java
+++ Fersona/src/main/java/com/smhrd/model/Want.java
@@ -1,6 +1,42 @@
 package com.smhrd.model;
 
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@NoArgsConstructor // 기본 생성자
+@RequiredArgsConstructor // final, @NonNUll
+@AllArgsConstructor // 모든 파라미터 받는 생성자
 public class Want {
 
+	@NonNull
+	private String want_id;
+	private String want_name;
+	private String want_age;
+	private String want_gen;
+	private String want_img;
+	private String rep_cate;
+	private String admin_adr1;
+	private String want_open;
+	private String want_char;
 	
+	private BigDecimal num;
+
+	
+	public Want(@NonNull String want_id, String want_name, String want_age, String want_gen, String rep_cate,
+			String want_open) {
+		this.want_id = want_id;
+		this.want_name = want_name;
+		this.want_age = want_age;
+		this.want_gen = want_gen;
+		this.rep_cate = rep_cate;
+		this.want_open = want_open;
+	}
+	
+
 }
--- Fersona/src/main/java/com/smhrd/model/WantDAO.java
+++ Fersona/src/main/java/com/smhrd/model/WantDAO.java
@@ -1,9 +1,59 @@
 package com.smhrd.model;
 
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.smhrd.database.SqlSessionManager;
+
 public class WantDAO {
 
-	
-	
+	// 세션을 생성해줄 수 있는 factory 생성
+	SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
 
+	// 세션 생성(sql 실행, close(), 연결(connection))
+	SqlSession sqlSession = sqlSessionFactory.openSession();
+
+	// 검색조회 메서드
+	public Want selectMember(Want vo) {
+		Want wantMember = null;
+		try {
+			wantMember = sqlSession.selectOne("com.smhrd.model.WantDAO.selectMember", vo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+		return wantMember;
+	}
+
+	// 전체내용 가져오기
+	public List<Want> selectAllList() {
+		List<Want> list = null;
+		try {
+			list = sqlSession.selectList("com.smhrd.model.WantDAO.selectAllList");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+		return list;
+	}
+	
+	// page 카운트
+	public List<Want> selectAllListPage(int num) {
+		List<Want> list = null;
+		try {
+			list = sqlSession.selectList("com.smhrd.model.WantDAO.selectAllListPage", num);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+		return list;
+	}
+	
 	
 }
